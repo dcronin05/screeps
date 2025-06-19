f94fc719c64f7a8200d57bdae7ad3e9476ae5aaa
Daniel Cronin <daniel@dcron.in>
2025-06-18 23:40:12 -0500

    force harvest from specific node

--- dist/role.harvester.js
+++ dist/role.harvester.js
@@ -4,8 +4,9 @@ var roleHarvester = {
     run: function(creep) {
 	    if(creep.store.getFreeCapacity() > 0) {
             var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+            var source = creep.room.getObjectById('5bbcac5f9099fc012e63559f');
+            if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
             }
         }
         else {
