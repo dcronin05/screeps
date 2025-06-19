660a6c4c6ff902825dcc01edcc94bab243c6fd00
Daniel Cronin <daniel@dcron.in>
2025-06-19 00:14:43 -0500

    fixe repairer module

--- dist/main.js
+++ dist/main.js
@@ -1,6 +1,7 @@
 var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
+var roleRepairer = require('role.repairer');
 
 module.exports.loop = function () {
 
--- dist/role.repairer.js
+++ dist/role.repairer.js
@@ -1,18 +1,18 @@
-var roleBuilder = {
+var roleRepairer = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
 
-	    if(creep.memory.building && creep.store[RESOURCE_ENERGY] == 0) {
-            creep.memory.building = false;
+	    if(creep.memory.repairing && creep.store[RESOURCE_ENERGY] == 0) {
+            creep.memory.repairing = false;
             creep.say('🔄 harvest');
 	    }
-	    if(!creep.memory.building && creep.store.getFreeCapacity() == 0) {
-	        creep.memory.building = true;
-	        creep.say('🚧 build');
+	    if(!creep.memory.repairing && creep.store.getFreeCapacity() == 0) {
+	        creep.memory.repairing = true;
+	        creep.say('🚧 repair');
 	    }
 
-	    if(creep.memory.building) {
+	    if(creep.memory.repairing) {
 	        var targets = creep.room.find(FIND_STRUCTURES);
             if(targets.length) {
                 if(creep.repair(targets[0]) == ERR_NOT_IN_RANGE) {
@@ -29,4 +29,4 @@ var roleBuilder = {
 	}
 };
 
-module.exports = roleBuilder;
+module.exports = roleRepairer;
