c73a7960319e9d360608a5870e6d1fd9c3b74a52
Daniel Cronin <daniel@dcron.in>
2025-06-19 02:42:28 -0500

    update

--- dist/role.repairer.js
+++ dist/role.repairer.js
@@ -22,14 +22,14 @@ var roleRepairer = {
 					creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
 				}
 			}
-			// else {
-			// 	var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-            // 	if(targets.length) {
-            //     	if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
-            //         	creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#jjjjjj'}});
-            //     	}
-          	// 	}
-			// }
+			else {
+				var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+            	if(targets.length) {
+                	if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
+                    	creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#jjjjjj'}});
+                	}
+          		}
+			}
 	    }
 	    else {
 	        var sources = creep.room.find(FIND_SOURCES);
