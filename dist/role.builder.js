c505ec486615b761d38a70da874f7482ec4d95fb
Daniel Cronin <daniel@dcron.in>
2025-06-19 00:59:03 -0500

    fix stuck builder

--- dist/role.builder.js
+++ dist/role.builder.js
@@ -14,7 +14,7 @@ var roleBuilder = {
 
 	    if(creep.memory.building) {
 	        var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-			targets.sort((a,b) => creep.pos.getRangeTo(a) - creep.pos.getRangeTo(b));
+			targets.sort((a,b) => creep.pos.getRangeTo(b) - creep.pos.getRangeTo(a));
             if(targets.length) {
                 if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#jjjjjj'}});
