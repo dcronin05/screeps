621330f9ac44d215a75775c56795744d810ba5d0
Daniel Cronin <daniel@dcron.in>
2025-06-19 01:38:59 -0500

    testing

--- dist/role.builder.js
+++ dist/role.builder.js
@@ -17,8 +17,8 @@ var roleBuilder = {
 			targets.sort((a,b) => (a.progressTotal - a.progress) - (b.progressTotal - b.progress));
             if(targets.length) {
 				for(var target in targets[0]) {
+					console.log(target.structureType);
 					if(target.structureType) {
-						console.log(target.structureType);
 						if(creep.build(target) == ERR_NOT_IN_RANGE) {
 							creep.moveTo(target, {visualizePathStyle: {stroke: '#jjjjjj'}});
 						}
