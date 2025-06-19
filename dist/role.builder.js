e4533d067b0312afc3023bc166c18cbe26bd064c
Daniel Cronin <daniel@dcron.in>
2025-06-19 00:25:48 -0500

    fix biulders

--- dist/role.builder.js
+++ dist/role.builder.js
@@ -13,7 +13,7 @@ var roleBuilder = {
 	    }
 
 	    if(creep.memory.building) {
-	        var targets = creep.room.find(FIND_STRUCTURES);
+	        var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
             if(targets.length) {
                 if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#jjjjjj'}});
