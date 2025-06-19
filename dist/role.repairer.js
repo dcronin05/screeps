a641ab929014752e2fd2340ecfa6bae16982bf09
Daniel Cronin <daniel@dcron.in>
2025-06-19 03:45:44 -0500

    update

--- dist/role.repairer.js
+++ dist/role.repairer.js
@@ -17,7 +17,7 @@ var roleRepairer = {
 				filter: object => object.hits < 30000
 			});
 			targets.sort((a,b) => a.hitsMax - b.hitsMax);
-			console.log("Repairing " + targets[2].structureType + " ID: " + targets[2].id);
+			console.log("Repairing " + targets[2].structureType + " with ID: " + targets[2].id);
 			if(targets.length > 0) {
 				if(creep.repair(targets[2]) == ERR_NOT_IN_RANGE) {
 					creep.moveTo(targets[2], {visualizePathStyle: {stroke: '#7DDA58'}});
