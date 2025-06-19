fbc23541c8b18cc1d6bbc4b240e4bb7fc060a213
Daniel Cronin <daniel@dcron.in>
2025-06-19 04:03:38 -0500

    update

--- dist/role.builder.js
+++ dist/role.builder.js
@@ -26,8 +26,8 @@ var roleBuilder = {
 		}
 	    else {
 	        var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[1], {visualizePathStyle: {stroke: '#FFDE59'}});
+            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#FFDE59'}});
 				creep.say('Harvesting...');
             }
 	    }
