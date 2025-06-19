deaed4ba3e722e7bdcb223a50ddeea63942f7e3c
Daniel Cronin <daniel@dcron.in>
2025-06-19 04:16:01 -0500

    update

--- dist/role.harvester.js
+++ dist/role.harvester.js
@@ -20,7 +20,7 @@ var roleHarvester = {
             if(targets.length > 0) {
                 if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#7DDA58'}});
-                    creep.say('Harvesting...');
+                    creep.say('🔄');
                 }
             }
         }
