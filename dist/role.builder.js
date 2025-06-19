3158974b1f37d76cb8fa1f1acb5c0c822e78023d
Daniel Cronin <daniel@dcron.in>
2025-06-19 01:00:33 -0500

    fix builders

--- dist/role.builder.js
+++ dist/role.builder.js
@@ -14,8 +14,8 @@ var roleBuilder = {
 
 	    if(creep.memory.building) {
 	        var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-			targets.sort((a,b) => creep.pos.getRangeTo(b) - creep.pos.getRangeTo(a));
-			console.log(targets);
+			targets.sort((a,b) => creep.pos.getRangeTo(a) - creep.pos.getRangeTo(b));
+			// console.log(targets);
             if(targets.length) {
                 if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#jjjjjj'}});
