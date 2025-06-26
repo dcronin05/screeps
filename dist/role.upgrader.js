0674a84493b68e336e505b6cf1f1214690750394
Daniel Cronin <daniel@dcron.in>
2025-06-26 18:27:28 -0500

    update upgrader

--- dist/role.upgrader.js
+++ dist/role.upgrader.js
@@ -13,14 +13,14 @@ var roleUpgrader = {
 	    if(creep.memory.upgrading) {
             if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#060270'}});
-                creep.say('⚡');
+                creep.say('⚡');
             }
         }
         else {
             var sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#FFDE59'}});
-                creep.say('🔄');
+                creep.say('⚡🔄');
             }
         }
 	}
