77b23494db46d5e16a3ef6d54bdcad58fe29c6c2
Daniel Cronin <daniel@dcron.in>
2025-06-28 11:15:06 -0500

    update harvesters

--- dist/role.harvester.js
+++ dist/role.harvester.js
@@ -21,14 +21,17 @@ var roleHarvester = {
             });
             if(targets.length > 0) {
                 for (var target of targets) {
-                    if (target.structureType == STRUCTURE_TOWER) {
+                    while (target.structureType == STRUCTURE_TOWER) {
                         if (creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                             creep.moveTo(target, {visualizePathStyle: {stroke: '#7DDA58'}});
                             creep.say('🚚');
                         }
+                        if (target.store.getFreeCapacity(RESOURCE_ENERGY) == 0) {
+                            break; // Stop transferring if the tower is full
+                        }
                     }
                 }
-                
+
                 if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#7DDA58'}});
                     creep.say('🚚');
