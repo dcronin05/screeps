b167e96c2e08f054cdc1afc290895d1287c3792a
Daniel Cronin <daniel@dcron.in>
2025-06-27 19:39:40 -0500

    update repairer

--- dist/role.repairer.js
+++ dist/role.repairer.js
@@ -17,13 +17,13 @@ var roleRepairer = {
 					&& object.hits < object.hitsMax
 				)
 			});
-			targets.sort((a,b) => (a.hitsMax / a.hits) - (b.hitsMax / b.hits));
+			targets.sort((b,a) => (a.hitsMax / a.hits) - (b.hitsMax / b.hits));
 			if(Game.time % 2 == 0) {
 				console.log("Repairing " + targets[0].structureType + " with ID: " + targets[0].id + " with hits: " + targets[0].hits + " out of " + targets[0].hitsMax);
 			};
 			if(targets.length > 0) {
-				if(creep.repair(targets[2]) == ERR_NOT_IN_RANGE) {
-					creep.moveTo(targets[2], {visualizePathStyle: {stroke: '#7DDA58'}});
+				if(creep.repair(targets[0]) == ERR_NOT_IN_RANGE) {
+					creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#7DDA58'}});
 					creep.say('🚧');
 				}
 			}
