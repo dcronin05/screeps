22189b1ceb4cf4c4f5c9a31244071d7b2ef4f371
Daniel Cronin <daniel@dcron.in>
2025-06-28 12:43:34 -0500

    update harvesters

--- dist/role.harvester.js
+++ dist/role.harvester.js
@@ -2,7 +2,7 @@ var roleHarvester = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-        var sources = creep.room.find(FIND_SOURCES);
+        var sources = creep.room.find(FIND_SOURCES); 
         if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
             creep.moveTo(sources[0]);
         }
