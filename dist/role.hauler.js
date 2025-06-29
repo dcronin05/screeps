cc95a7033ef854cab359fbc426b3712bbf19acb4
Daniel Cronin <daniel@dcron.in>
2025-06-29 03:23:32 -0500

    update haulers

--- dist/role.hauler.js
+++ dist/role.hauler.js
@@ -15,8 +15,9 @@ var roleHauler = {
                     filter: (structure) => {
                         return (structure.structureType == STRUCTURE_TOWER ||
                             structure.structureType == STRUCTURE_EXTENSION || 
-                            structure.structureType == STRUCTURE_SPAWN) &&
-                            structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+                            structure.structureType == STRUCTURE_SPAWN)
+                            //  &&
+                            // structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
                     }
             });
             while(targets.length > 0) {
