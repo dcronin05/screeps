9e9b0dd718439bd12c758785d36bc508f6696e26
Daniel Cronin <daniel@dcron.in>
2025-06-30 10:43:44 -0500

    update hauler

--- dist/role.hauler.js
+++ dist/role.hauler.js
@@ -21,6 +21,7 @@ var roleHauler = {
             });
 
             if (targets.length > 0) {
+                console.log(targets);
                 // for (var target of targets) {
                 //     if (creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                 //         creep.moveTo(target, {visualizePathStyle: {stroke: '#7DDA58'}});
