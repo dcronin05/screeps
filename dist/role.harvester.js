bc9727ac6896358824051c3b033e34656f2570c0
Daniel Cronin <daniel@dcron.in>
2025-06-30 11:14:18 -0500

    update harvesters

--- dist/role.harvester.js
+++ dist/role.harvester.js
@@ -13,13 +13,16 @@ var roleHarvester = {
                 creep.memory.source = 1; // Switch to the second source if the first is empty
             }
 	    }
-	    if(!creep.memory.source == 0) {
+	    else if(creep.memory.source == 1) {
             if(creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[1]);
             }
             if(sources[1].energy == 0) {
                 creep.memory.source = 0; // Switch to the first source if the second is empty
             }
+        }
+        else {
+            creep.memory.source = 0; // Default to the first source if no valid source is set
         }
 
     }
