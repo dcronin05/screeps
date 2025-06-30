4f7a52ac61e12bd6861cb047eba8b5793bbff3b9
Daniel Cronin <daniel@dcron.in>
2025-06-30 13:35:10 -0500

    update haulers

--- dist/role.hauler.js
+++ dist/role.hauler.js
@@ -20,14 +20,11 @@ var roleHauler = {
                     }
             });
 
+            targets.sort((a,b) => a.store.getFreeCapacity(RESOURCE_ENERGY) - b.store.getFreeCapacity(RESOURCE_ENERGY));
+
             if (targets.length > 0) {
-                if(creep.transfer(targets[-1], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[-1], {visualizePathStyle: {stroke: '#7DDA58'}});
-                }
-                if (creep.transfer(targets[-1], RESOURCE_ENERGY) == ERR_FULL) {
-                    if(creep.transfer(targets[1], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                        creep.moveTo(targets[1], {visualizePathStyle: {stroke: '#7DDA58'}});
-                    }
+                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#7DDA58'}});
                 }
             }
         } else {
