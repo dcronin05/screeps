f03c9469627370fb914a447ba1404165ca438ad2
Daniel Cronin <daniel@dcron.in>
2025-07-01 10:04:35 -0500

    update haulers

--- dist/role.hauler.js
+++ dist/role.hauler.js
@@ -52,7 +52,8 @@ var roleHauler = {
                     if (creep.withdraw(energy[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                         creep.moveTo(energy[0], {visualizePathStyle: {stroke: '#FFDE59'}});
                     }
-                } else if (energy[0].type == TOMBSTONE) {
+
+                } else if (energy[0].type == 'Tombstone') {
                     if (creep.withdraw(energy[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                         creep.moveTo(energy[0], {visualizePathStyle: {stroke: '#FFDE59'}});
                     }
