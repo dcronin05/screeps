bc70aa8f6aae329e6060c1cc46dfd61f52804345
Daniel Cronin <daniel@dcron.in>
2025-07-01 20:45:56 -0500

    update repairers

--- dist/main.js
+++ dist/main.js
@@ -135,9 +135,9 @@ module.exports.loop = function () {
         if(creep.memory.role == 'builder') {
             roleBuilder.run(creep);
         }
-        // if(creep.memory.role == 'repairer') {
-        //     roleRepairer.run(creep);
-        // }
+        if(creep.memory.role == 'repairer') {
+            roleRepairer.run(creep);
+        }
         if(creep.memory.role == 'hauler') {
             roleHauler.run(creep);
         }
