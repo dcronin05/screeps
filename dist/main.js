1aefa6ce02894bfe00ff76ffbcd3b719d31c08ed
Daniel Cronin <daniel@dcron.in>
2025-07-01 21:11:41 -0500

    update towers

--- dist/main.js
+++ dist/main.js
@@ -16,7 +16,7 @@ module.exports.loop = function () {
     if(tower) {
         var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
         var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
-            filter: (structure) => (structure.hits < structure.hitsMax) && structure.hits < 3001
+            filter: (structure) => (structure.hits < structure.hitsMax) && structure.hits < 20001
         });
         
         if(closestHostile) {
@@ -75,7 +75,7 @@ module.exports.loop = function () {
         Game.spawns['Spawn1'].spawnCreep([CARRY,MOVE], newName, 
             {memory: {role: 'hauler'}});
     }
-    if(haulers.length < 5) {
+    if(haulers.length < 10) {
         var newName = 'Hauler' + Game.time;
         if(Game.time % 20 == 0) {
             console.log('Spawning new hauler: ' + newName);
@@ -91,7 +91,7 @@ module.exports.loop = function () {
         console.log(Game.spawns['Spawn1'].spawnCreep([WORK,WORK,WORK,CARRY,CARRY,CARRY,CARRY,MOVE,MOVE], newName, 
             {memory: {role: 'repairer'}}));
     }
-    if(upgraders.length < 10) {
+    if(upgraders.length < 3) {
         var newName = 'Upgrader' + Game.time;
         if(Game.time % 20 == 0) {
             console.log('Spawning new upgrader: ' + newName);
