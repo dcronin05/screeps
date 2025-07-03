b99ba2370c267acca4392bdacc1fd034a5ae5db2
Daniel Cronin <daniel@dcron.in>
2025-07-03 16:40:13 -0500

    update harvesters

--- dist/role.harvester.js
+++ dist/role.harvester.js
@@ -8,8 +8,6 @@ var roleHarvester = {
         if (creep.ticksToLive > 1000) { 
             creep.memory.dying = false; 
         }
-        console.log('spawn used cap = ' + Game.spawns['Spawn1'].store.getUsedCapacity(RESOURCE_ENERGY));
-
         if (creep.ticksToLive < 500 && Game.spawns['Spawn1'].store.getUsedCapacity(RESOURCE_ENERGY) > 200) { 
             creep.memory.dying = true; 
             console.log(creep.name + ' is dying');
