d66151aa528fca050ce8d8c95f4d980da50d3b48
Daniel Cronin <daniel@dcron.in>
2025-07-02 23:34:27 -0500

    update repairers

--- dist/role.repairer.js
+++ dist/role.repairer.js
@@ -15,7 +15,7 @@ var roleRepairer = {
 			var targets = creep.room.find(
 				FIND_STRUCTURES, { 
 					filter: (structure) => { 
-						return structure.hits < 3000 && structure.hits < structure.hitsMax 
+						return structure.hits < 30000 && structure.hits < structure.hitsMax 
 					}
 				}
 			);
