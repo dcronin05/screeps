7161189cc85296f1179e8da8d30e1f629f6ebcda
Daniel Cronin <daniel@dcron.in>
2025-07-03 15:16:41 -0500

    update haulers

--- dist/role.hauler.js
+++ dist/role.hauler.js
@@ -60,7 +60,9 @@ var roleHauler = {
             }
         } 
         else if (!creep.memory.dying) {
-            var energy = creep.room.find(FIND_DROPPED_RESOURCES);
+            var energy = creep.room.find(FIND_DROPPED_RESOURCES, {
+                filter: (e) => { return ( e.pos.x != 1 && e.pos.x != 48 && e.pos.y != 48 )}
+            });
             
             energy = energy.concat(creep.room.find(FIND_STRUCTURES, {
                 filter: (structure) => { return (
