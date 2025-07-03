c3ced0bf5dcc5199c5ef09a113f4818e535b3c54
Daniel Cronin <daniel@dcron.in>
2025-07-03 14:39:25 -0500

    update haulers

--- dist/main.js
+++ dist/main.js
@@ -140,7 +140,9 @@ module.exports.loop = function () {
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
 
-        if (creep.memory.dying) { console.log(Game.spawns['Spawn1'].renewCreep(creep)); };
+        if (creep.memory.dying && creep.pos.getRangeTo(Game.spawns['Spawn1']) == 0) { 
+            console.log(Game.spawns['Spawn1'].renewCreep(creep)); 
+        }
 
         if(creep.memory.role == 'harvester') {
             roleHarvester.run(creep);
