398c51f480f41a00b0a319133d47109071ecff73
Daniel Cronin <daniel@dcron.in>
2025-07-02 22:48:25 -0500

    update haulers

--- dist/role.hauler.js
+++ dist/role.hauler.js
@@ -29,8 +29,10 @@ var roleHauler = {
 
             if (targets.length > 0 && targets[0].structureType != STRUCTURE_STORAGE) {
                 target = creep.pos.findClosestByRange(targets);
+
                 for (var t of targets) { 
                     if (target.structureType == STRUCTURE_TOWER && target.store.getFreeCapacity(RESOURCE_ENERGY) > 199) {
+                        console.log('Found a tower with enough capacity: ' + target.id);
                         target = t;
                     }
                 }
