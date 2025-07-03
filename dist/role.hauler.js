5794a7e770ffd15cfad745bd4a184f466bef52e9
Daniel Cronin <daniel@dcron.in>
2025-07-03 00:49:34 -0500

    update haulers

--- dist/role.hauler.js
+++ dist/role.hauler.js
@@ -66,7 +66,7 @@ var roleHauler = {
                 target = creep.pos.findClosestByRange(energy);
                 
                 for (var ruin of energy) {
-                    console.log(ruin.type + ' ' + ruin.id + ' ' + ruin.store + ' ' + ruin.pos.x + 'x' + ruin.pos.y);
+                    console.log(ruin.type + ' ' + ruin.id + ' ' + ruin.store.getUsedCapacity() + ' ' + ruin.pos.x + 'x' + ruin.pos.y);
                     if (ruin.type == 'Ruin' && ruin.store.getUsedCapacity() > 0) {
                         target = ruin;
                         console.log('ruin found: ' + target.id + ' ' + target.pos.x + 'x' + target.pos.y);
