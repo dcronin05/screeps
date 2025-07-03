f10df19da2ff186e2e31f99ae6b3b0f1eac23f10
Daniel Cronin <daniel@dcron.in>
2025-07-03 11:10:21 -0500

    update towers

--- dist/main.js
+++ dist/main.js
@@ -16,7 +16,7 @@ module.exports.loop = function () {
     if(tower) {
         var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
         var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
-            filter: (structure) => (structure.hits < structure.hitsMax) && structure.hits < 3001
+            filter: (structure) => (structure.hits < structure.hitsMax) && structure.hits < 50001
         });
         
         if(closestHostile) {
