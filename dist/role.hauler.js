5edd5dfad442431fdf528f95cd4ca4a0e4d8c7b3
Daniel Cronin <daniel@dcron.in>
2025-07-03 16:34:29 -0500

    update repairing

--- dist/role.hauler.js
+++ dist/role.hauler.js
@@ -129,7 +129,7 @@ var roleHauler = {
         else {
             creep.say('🏥')
             if (creep.pos.getRangeTo(Game.spawns['Spawn1']) > 0) {
-                creep.moveTo(Game.flags['Renew']);
+                creep.moveTo(Game.spawns['Spawn1']);
                 console.log(creep.name + ' ' + creep.pos.getRangeTo(Game.spawns['Spawn1']) + ' away from spawn');
             }
         }
