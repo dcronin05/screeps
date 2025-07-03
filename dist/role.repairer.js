798033c9058b21ab779def566c3d09a09c068710
Daniel Cronin <daniel@dcron.in>
2025-07-02 23:12:11 -0500

    update repairers

--- dist/role.repairer.js
+++ dist/role.repairer.js
@@ -13,8 +13,8 @@ var roleRepairer = {
 	    }
 
 	    if(creep.memory.repairing) {
-	        var targets = creep.room.find(FIND_STRUCTURES);
-			targets.sort((b,a) => (a.hitsMax / a.hits) - (b.hitsMax / b.hits));
+	        var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+			// targets.sort((b,a) => (a.hitsMax / a.hits) - (b.hitsMax / b.hits));
 			if(Game.time % 2 == 0 && targets.length > 0) {
 				console.log("Repairing " + targets[0].structureType + " with ID: " + targets[0].id + " with hits: " + targets[0].hits + " out of " + targets[0].hitsMax);
 			};
