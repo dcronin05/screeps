227731369ba4c20ef355b208e28bec17437a8986
Daniel <daniel@dcron.in>
2025-07-10 09:28:53 -0500

    update spawn quotas

--- dist/main.js
+++ dist/main.js
@@ -75,7 +75,7 @@ module.exports.loop = function () {
                 {memory: {role: 'hauler'}})
         );
     }
-    else if(haulers.length < 2) {
+    else if(haulers.length < 3) {
         var newName = 'Hauler' + Game.time;
         if(Game.time % 20 == 0) {
             console.log('Spawning new hauler: ' + newName);
